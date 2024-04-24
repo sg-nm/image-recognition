e86a9d6369e89bef129a8220b0594c85c0477925
Masanori Suganuma <ykhmprsi1029@gmail.com>
2024-04-24 17:23:41 +0900

    Add files via upload

--- chap08/run_eval.sh
+++ chap08/run_eval.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-ID=`date "+%m%d_%H%M_%S"`
-
-export NCCL_SOCKET_IFNAME=bond0
-export NCCL_P2P_LEVEL=1
-
 python evaluate.py \
 --cfg ./configs/vit_s.yml \
 --resume "./trained_model/epoch_latest.pt"
