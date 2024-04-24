7981078e4a19dbde4416ba44eb110d2085041716
Masanori Suganuma <ykhmprsi1029@gmail.com>
2024-04-24 17:25:08 +0900

    Add files via upload

--- chap08/run.sh
+++ chap08/run.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-ID=`date "+%m%d_%H%M_%S"`
-
 export NCCL_SOCKET_IFNAME=bond0
 export NCCL_P2P_LEVEL=1
 
 NUM_NODES=1
 WORKERS_PER_NODE=4
 
+## for multi-gpu training
 torchrun \
 --standalone \
 --nnodes=$NUM_NODES \
 --nproc_per_node=$WORKERS_PER_NODE \
 train_clip.py \
 --cfg ./configs/vit_s.yml \
---output_dir "logs/log-"$ID \
 
+## for single GPU training
 # python train_clip.py \
 # --cfg ./configs/vit_s.yml \
 # --output_dir "logs/log-"$ID \
